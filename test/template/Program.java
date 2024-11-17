36b59c1c10de9ef87e4b2d77ea6eda42a5a092ab
IzanamiiDevv <rafaeloli596@gmail.com>
2024-11-17 20:40:49 +0800

    add test field

--- test/template/Program.java
+++ test/template/Program.java
@@ -1,5 +1,9 @@
-package test.template;
+import JLua.JLua;
 
 public class Program {
-    
+    public static void main(String[] args) {
+        JLua luaStream = new JLua("template.lua");
+
+        luaStream.p_run("echo", "string", "Hello World", 1, Void.class);
+    }
 }
